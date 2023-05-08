9a1cc40d9f6f2d368d02bd029968db07c1033d91
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-07 20:55:56 -0500

    Delete button to change input behaviour

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,23 +2,22 @@ import { useContext, useState } from "react";
 import UserContext from "./UserContex";
 
 const Form = () => {
+	const [name, setName] = useState("")
+	const userContext = useContext(UserContext)
 
-
-
-
-
+	const handleChange = (e) => {
+		setName(e.target.value)
+		userContext.setName(e.target.value)
+	}
 
 	return (
 		<form>
 			<input
 				type="text"
 				placeholder="John Doe"
-			/* value={name} */
-			/* onChange */
+				value={name}
+				onChange={(handleChange)}
 			/>
-			<button type="submit">Change name</button>
-
-
 		</form>
 	)
 }
