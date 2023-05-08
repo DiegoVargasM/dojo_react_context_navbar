437b68d443797b27d7972e5fb201468644c1206d
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-07 21:10:49 -0500

    Add navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import { useContext } from "react";
 import UserContext from "./UserContext";
+import "./navbar.css";
 
 const Navbar = () => {
   const userContext = useContext(UserContext);
 
-  return <div>Hello, {userContext.name}</div>;
+  return (
+    <div className="navbar">
+      Hello,{" "}
+      {userContext.name === "" ? "no identificado!" : userContext.name + "!"}
+    </div>
+  );
 };
 
 export default Navbar;
